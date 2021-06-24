dfb0f367b3fa61298a1862084c176422248f963b
Amirmahdi <tafreshi.amirmahdi@protonmail.com>
2021-06-24 23:58:04 +0430

    add simplejwt for auth

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "rest_framework",
+    'rest_framework',
+    'rest_framework_simplejwt',
+
 ]
 
 MIDDLEWARE = [
